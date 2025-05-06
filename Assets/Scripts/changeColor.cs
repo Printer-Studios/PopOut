5fa419c6aac1334fa33b9406396aa85afabb4a8a
ElBuenTocino <paumm8@gmail.com>
2025-05-06 15:37:33 +0200

    i commit
    ge tout

--- Assets/Scripts/changeColor.cs
+++ Assets/Scripts/changeColor.cs
@@ -6,6 +6,7 @@ public class changeColor : MonoBehaviour
 
     public Color color1;
     public Color color2;
+    public Color newColor;
     [Range(0f, 1f)]
     public float tc; // Number between 0 and 1 that determines how much does the "Pop" original color approach the second color.
     SpriteRenderer spriteRenderer;
@@ -19,7 +20,8 @@ public class changeColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        tc = Absorb.currentAmmo / Absorb.maxCapacity;
-        spriteRenderer.color = Color.Lerp(new Color(color1.r, color1.g, color1.b, HealthBehaviour.alpha), new Color(color2.r, color2.g, color2.b, HealthBehaviour.alpha), tc);
+        //tc = Absorb.currentAmmo / Absorb.maxCapacity;
+        newColor = Color.Lerp(new Color(color1.r, color1.g, color1.b, HealthBehaviour.alpha), new Color(color2.r, color2.g, color2.b, HealthBehaviour.alpha), tc);
+        spriteRenderer.color = newColor;
     }
 }
