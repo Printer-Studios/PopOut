b5225eaea914483f1b9376372c4666c893e706f0
serar3 <sergio.alsa33@gmail.com>
2025-05-07 16:41:56 +0200

    level1 and hearts created

---
+++ Assets/Scripts/changeColorHeart.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeColorHeart : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Color color1;
+    public Color color2;
+    public Color newColor;
+    [Range(0f, 1f)]
+    public float tc; // Number between 0 and 1 that determines how much does the "Pop" original color approach the second color.
+    Image heart;
+    public HealthBehaviour HealthBehaviour;
+    public AbsorbBehaviour1 Absorb;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        heart = GetComponent<Image>();
+    }
+    void Update()
+    {
+        tc = Absorb.currentAmmo / Absorb.maxCapacity;
+        newColor = Color.Lerp(new Color(color1.r, color1.g, color1.b, HealthBehaviour.alpha), new Color(color2.r, color2.g, color2.b, HealthBehaviour.alpha), tc);
+        heart.color = newColor;
+    }
+}
