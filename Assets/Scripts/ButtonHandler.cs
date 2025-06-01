76b9dd15e09f91d76df354d35f482167d170786a
serar3 <sergio.alsa33@gmail.com>
2025-06-01 02:11:03 +0200

    tututututututorial gooooood

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -6,13 +6,19 @@ public class ButtonHandler : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        buttonIsPressed = true;
-        Debug.Log(buttonIsPressed);
+        //if(collision.gameObject.tag == "floor")
+        //{
+            buttonIsPressed = true;
+            Debug.Log(buttonIsPressed);
+        //}
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        buttonIsPressed = false;
+        //if (collision.gameObject.tag == "floor")
+        //{
+            buttonIsPressed = false;
+        //}
     }
 
 
