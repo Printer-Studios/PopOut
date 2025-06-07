d1bdb7dfbe7e8b5da59d0a53c66f68f80db82257
serar3 <sergio.alsa33@gmail.com>
2025-06-07 20:52:37 +0200

    navegacion god i goal god
    abro menu con mando i hace wawawawa

---
+++ Assets/Scripts/MainMenuBehaviour.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+    public GameObject sistemEvent;
+    [SerializeField] private PoiSO[] poiSOs;
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenPanel(GameObject panel)
+    {
+        panel.SetActive(!panel.activeSelf);
+        sistemEvent.SetActive(!sistemEvent.activeSelf);
+    }
+
+    public void ResetPlayerPrefs()
+    {
+        for (int i = 1; i < poiSOs.Length; i++) //This is to avoid having random levels already unlocked.
+        {
+            poiSOs[i].isUnlocked = false;
+        }
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+}
